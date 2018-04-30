cfa72733f3546690a98f01d2dde2dcb4062c4d39
Caesar <caesar.chodun@gmail.com>
2018-04-30 21:30:35 +0200

    Enables device enumeration and storage.
    As stated in title.

--- src/core/managers/HardwareManager.java
+++ src/core/managers/HardwareManager.java
@@ -3,12 +3,17 @@ package core.managers;
 import static org.lwjgl.vulkan.KHRSurface.VK_KHR_SURFACE_EXTENSION_NAME;
 import static org.lwjgl.system.MemoryUtil.*;
 import static org.lwjgl.glfw.GLFWVulkan.*;
+import static org.lwjgl.vulkan.VK10.*;
 import static org.lwjgl.glfw.GLFW.*;
 
 import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 
 import org.lwjgl.vulkan.VkApplicationInfo;
 import org.lwjgl.vulkan.VkInstance;
+
+import core.employees.PhysicalDevice;
+
 import org.lwjgl.PointerBuffer;
 
 /**
@@ -18,9 +23,12 @@ import org.lwjgl.PointerBuffer;
 public class HardwareManager {
 
 	public static final boolean validation = Boolean.parseBoolean(System.getProperty("vulkan.validation", "false"));
+	public static final ByteBuffer[] DEFAULT_VALIDATION_LAYERS = {memUTF8("VK_LAYER_LUNARG_standard_validation"), memUTF8("VK_LAYER_LUNARG_object_tracker")};
+	public static final ByteBuffer[] DEFAULT_EXTENSIONS = {memUTF8(VK_KHR_SURFACE_EXTENSION_NAME)};
 	
 	private static VkInstance instance = null;
 	private static PointerBuffer requiredExtensions = null;
+	public static PhysicalDevice[] devices;
 	
 	/**
 	 * <h5>Description:</h5>
@@ -39,17 +47,47 @@ public class HardwareManager {
 	
 	/**
 	 * <h5>Description:</h5>
+	 * <p>Enumerate available physical devices.</p>
+	 * <p><b>Note:</b> This method should be invoked only once.</p>
+	 */
+	public static void enumeratePhysicalDevices() {
+		IntBuffer dev_count = memAllocInt(1);
+		int err = vkEnumeratePhysicalDevices(instance, dev_count, null);
+		if(err < 0)
+			throw new AssertionError("Could not enumerate physical devices: " + Util.translateVulkanError(err));
+		
+		int devCount = dev_count.get(0);
+		PointerBuffer pDevices = memAllocPointer(devCount);
+		
+		err = vkEnumeratePhysicalDevices(instance, dev_count, pDevices);
+		if(err < 0)
+			throw new AssertionError("Could not get physical devices: " + Util.translateVulkanError(err));
+		
+		devices = new PhysicalDevice[devCount];
+		for(int i = 0; i < devCount; i++)
+			devices[i] = new PhysicalDevice(pDevices.get(i), instance);
+		
+		memFree(dev_count);
+		memFree(pDevices);
+	}
+	
+	/**
+	 * <h5>Description:</h5>
 	 * <p>Creates default VkInstance.</p>
 	 * @see {@link org.lwjgl.vulkan.VkInstance}
 	 */
-	public static void createDefaultInstance() {
-		ByteBuffer[] defaultValidationLayers = {memUTF8("VK_LAYER_LUNARG_standard_validation"), memUTF8("VK_LAYER_LUNARG_object_tracker")};
-		ByteBuffer[] defaultExtensions = {memUTF8(VK_KHR_SURFACE_EXTENSION_NAME)};
-		
-		createCustomInstance(EngineVersioning.getAppInfo(), defaultValidationLayers, defaultExtensions);
-		
-		Util.freeBuffers(defaultValidationLayers);
-		Util.freeBuffers(defaultExtensions);
+	public static void createDefaultInstance() {		
+		createCustomInstance(EngineVersioning.getAppInfo(), DEFAULT_VALIDATION_LAYERS, DEFAULT_EXTENSIONS);
+	}
+	
+	/**
+	 * <h5>Description:</h5>
+	 * <p>Destroys properties contained by <b><i>PhysicalDevice</i></b>(for every physical device acquired before).</p>
+	 * @see {@link core.employees.PhysicalDevice}
+	 */
+	public static void destroyPhysicalDevicesProperties() {
+		for(int i = 0; i < devices.length; i++)
+			devices[i].destroyProperties();
 	}
 	
 	/**
