54a9b8fdfffb8bf7edb4653f87aeeaa6d6a50e54
Caesar <caesar.chodun@gmail.com>
2018-05-22 21:49:19 +0200

    Create ColorFormatAndSpace.
    This is a helper class.

---
+++ src/core/employees/ColorFormatAndSpace.java
@@ -0,0 +1,12 @@
+package core.employees;
+
+public class ColorFormatAndSpace {
+
+	public int colorFormat;
+	public int colorSpace;
+	
+	public ColorFormatAndSpace(int colorFormat, int colorSpace) {
+		this.colorFormat = colorFormat;
+		this.colorSpace = colorSpace;
+	}
+}
